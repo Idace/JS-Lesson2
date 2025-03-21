e09fca366980bdf0b2a6c5225d7bc0ad0a366ed6
Idace <idaebeling@gmail.com>
2025-03-21 09:55:43 +0100

    changed prompt

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
-let inputPrice = prompt("Enter a price, for example $49,99");
+let inputPrice = prompt("Enter a price:");
 
 let price = parseFloat(inputPrice.replace("$",""));
 
 let discountedPrice = price * 0.9;
 
-console.log(`Your new price is: $${discountedPrice.toFixed(2)}`);
+console.log(`Your new price is: $${discountedPrice.toFixed(2)}`);456.3
